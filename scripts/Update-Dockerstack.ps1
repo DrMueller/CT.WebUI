08a04fe54e4dfef5dc1bdae08161ba37959a013c
Matthias Mueller <matthiasm@live.de>
2018-11-09 14:37:31 +0100

    WIP

---
+++ scripts/Update-Dockerstack.ps1
@@ -0,0 +1,11 @@
+﻿param (
+  [string]$repositoryLocalPath, 
+  [string]$imageName 
+) 
+
+$filePath= Join-Path -Path $repositoryLocalPath -ChildPath "\docker-stack.yml"
+
+$environmentContent = Get-Content $filePath
+$replaced = $environmentContent -replace '__ImageName__', $imageName
+
+Set-Content -Path $filePath -value $replaced

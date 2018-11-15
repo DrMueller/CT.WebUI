0bce58e48975eee719b497f516e559508b4d2b9c
Matthias Mueller <matthiasm@live.de>
2018-11-15 13:20:52 +0100

    WIP

--- scripts/Update-EnvironmentConfig.ps1
+++ scripts/Update-EnvironmentConfig.ps1
@@ -4,13 +4,13 @@
 ) 
 
 
-$filePath= Join-Path -Path $repositoryLocalPath -ChildPath "\src\environments\environment.prod.ts"
+# $filePath= Join-Path -Path $repositoryLocalPath -ChildPath "\src\environments\environment.prod.ts"
 
-Write-Host "Repository LocalPath: $repositoryLocalPath"
-Write-Host "Environment-File: $filePath"
-Write-Host "Build Number: $buildNumber"
+# Write-Host "Repository LocalPath: $repositoryLocalPath"
+# Write-Host "Environment-File: $filePath"
+# Write-Host "Build Number: $buildNumber"
 
-$environmentContent = Get-Content $filePath
-$replaced = $environmentContent -replace '__BuildNumber__', 'buildNumber'
+# $environmentContent = Get-Content $filePath
+# $replaced = $environmentContent -replace '__BuildNumber__', 'buildNumber'
 
-Set-Content -Path $filePath -value $replaced
+# Set-Content -Path $filePath -value $replaced
